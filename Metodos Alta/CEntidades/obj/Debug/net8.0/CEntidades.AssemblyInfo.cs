e01b8201d0c035d97888f3c6283549cd0d5667d0
Leandro Bolzon <leosilva1711@gmail.com>
2024-08-27 18:52:13 -0300

    Volviendo a pasar la libreria de datos

--- Metodos Alta/CEntidades/obj/Debug/net8.0/CEntidades.AssemblyInfo.cs
+++ Metodos Alta/CEntidades/obj/Debug/net8.0/CEntidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CEntidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eca3362ce50966a8160cd6ae68cedf7828eefbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1af6d02dfaf1038c2049c61f6344a373352308e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CEntidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CEntidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Metodos Alta/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Metodos Alta/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69dd5c5b994d56ba7ba234dd3b5d8c579a961fcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1af6d02dfaf1038c2049c61f6344a373352308e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
