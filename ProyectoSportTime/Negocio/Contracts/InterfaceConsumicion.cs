d64ecc0cdca29150ed8111b4768bb2ded1d864e6
Mateo Ferrero <mateo.ferrero.ices@gmail.com>
2024-09-15 17:46:48 -0300

    Interfaces

---
+++ ProyectoSportTime/Negocio/Contracts/InterfaceConsumicion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Contracts
+{
+    public interface InterfaceConsumicion
+    {
+        void AltaCliente(string nombre, int numeroTelefono);
+        void ModificarCliente(int clienteID, string nuevoNombre, int nuevoTelefono);
+        void BajaCliente(int clienteID);
+    }
+}
---
+++ ProyectoSportTime/Negocio/Contracts/InterfaceProductos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Contracts
+{
+    public interface InterfaceProductos
+    {
+        void AltaProducto(string tipo, string descripcion, int proveedorID);
+        void ModificarProducto(int productoID, string nuevoTipo, string nuevaDescripcion);
+        void BajaProducto(int productoID);
+    }
+}
