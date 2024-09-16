f4c17467ac67df15327e76e1138de65b63dbcd22
Mateo Ferrero <mateo.ferrero.ices@gmail.com>
2024-09-16 17:17:39 -0300

    .

--- ProyectoSportTime/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ ProyectoSportTime/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa388129fa54e5cf2453bf3e4f4b6fea2249d329")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9391e0f629f0c9273389a08f24c82ace625452c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
