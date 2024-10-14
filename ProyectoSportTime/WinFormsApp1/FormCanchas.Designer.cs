4bdb800dc694215b37410bd4823b81532c7eca05
Mateo Ferrero <mateo.ferrero.ices@gmail.com>
2024-10-14 20:12:10 -0300

    .

--- ProyectoSportTime/WinFormsApp1/FormCanchas.Designer.cs
+++ ProyectoSportTime/WinFormsApp1/FormCanchas.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormCanchas";
+            textBox1 = new TextBox();
+            buttonAgregar = new Button();
+            buttonModificar = new Button();
+            buttonEliminar = new Button();
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // buttonAgregar
+            // 
+            buttonAgregar.Location = new Point(14, 137);
+            buttonAgregar.Name = "buttonAgregar";
+            buttonAgregar.Size = new Size(75, 23);
+            buttonAgregar.TabIndex = 1;
+            buttonAgregar.Text = "Agregar";
+            buttonAgregar.UseVisualStyleBackColor = true;
+            // 
+            // buttonModificar
+            // 
+            buttonModificar.Location = new Point(95, 137);
+            buttonModificar.Name = "buttonModificar";
+            buttonModificar.Size = new Size(75, 23);
+            buttonModificar.TabIndex = 2;
+            buttonModificar.Text = "Modificar";
+            buttonModificar.UseVisualStyleBackColor = true;
+            // 
+            // buttonEliminar
+            // 
+            buttonEliminar.Location = new Point(176, 137);
+            buttonEliminar.Name = "buttonEliminar";
+            buttonEliminar.Size = new Size(75, 23);
+            buttonEliminar.TabIndex = 3;
+            buttonEliminar.Text = "Eliminar";
+            buttonEliminar.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Futbol", "", "Voley", "", "Basquet" });
+            comboBox1.Location = new Point(12, 67);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(297, 25);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(366, 190);
+            dataGridView1.TabIndex = 5;
+            // 
+            // FormCanchas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(690, 252);
+            Controls.Add(dataGridView1);
+            Controls.Add(comboBox1);
+            Controls.Add(buttonEliminar);
+            Controls.Add(buttonModificar);
+            Controls.Add(buttonAgregar);
+            Controls.Add(textBox1);
+            Name = "FormCanchas";
+            Text = "FormCanchas";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button buttonAgregar;
+        private Button buttonModificar;
+        private Button buttonEliminar;
+        private ComboBox comboBox1;
+        private DataGridView dataGridView1;
     }
 }
